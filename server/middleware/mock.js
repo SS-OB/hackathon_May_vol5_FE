aee05b03518f0bc45cf13dec87946ae923592c56
Fujiken0705 <kentofujita.new@gmail.com>
2024-05-17 23:59:59 +0900

    ホーム画面に登録したデータをリスト形式で表示する

--- server/middleware/mock.js
+++ server/middleware/mock.js
@@ -16,6 +16,34 @@ export default function (req, res, next) {
       diary: "昨日は疲れた",
       image_path: "https://www.example.com/image2.jpg",
     },
+    {
+      id: 3,
+      music_id: "example-music-id-3",
+      memory_date: "2023-05-14T09:12:33.001Z",
+      diary: "今日はリラックスできた",
+      image_path: "https://www.example.com/image3.jpg",
+    },
+    {
+      id: 4,
+      music_id: "example-music-id-4",
+      memory_date: "2023-05-13T09:12:33.001Z",
+      diary: "友達と楽しい時間を過ごした",
+      image_path: "https://www.example.com/image4.jpg",
+    },
+    {
+      id: 5,
+      music_id: "example-music-id-5",
+      memory_date: "2023-05-12T09:12:33.001Z",
+      diary: "新しいレストランを試した",
+      image_path: "https://www.example.com/image5.jpg",
+    },
+    {
+      id: 6,
+      music_id: "example-music-id-6",
+      memory_date: "2023-05-12T09:12:33.001Z",
+      diary: "WTTKにボコされた",
+      image_path: "https://www.example.com/image5.jpg",
+    },
   ];
 
   res.setHeader("Content-Type", "application/json");
